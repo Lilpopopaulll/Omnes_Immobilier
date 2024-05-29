eb128c37d617dea3f9a0922a672814ba447aee11
Lilpopopaulll <paul.drochon@gmail.com>
2024-05-29 23:04:21 +0200

    connexion php

--- connexion.php
+++ connexion.php
@@ -1,6 +1,27 @@
 <?php
+// Identifier le nom de base de données
+$database = "omnes_immobilier";
+
+// Connectez-vous dans votre BDD
+// Votre serveur = localhost | votre login = root | votre mot de passe = '' (rien)
+$db_handle = mysqli_connect('localhost', 'root', '', $database);
+
+if ($db_handle) {
+
 if(isset($_POST['login'])) {
     $id = $_POST['id'];
-    $pdp = $_POST['password'];
-    echo $id;
+    $mdp = $_POST['password'];
+    if($id !="" && $mdp !="") {
+        $req = $db_handle->query("SELECT * FROM users WHERE id = '$id' AND mdp ='$mdp'");
+        $rep = mysqli_fetch_assoc($req);
+        if ($rep) {
+            echo "Vous êtes connecté";
+        } else {
+            echo "Vous n'êtes pas connecté";
+        }
+    }
 }
+}
+    
+
+?>
